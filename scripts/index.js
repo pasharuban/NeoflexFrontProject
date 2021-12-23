d319869f661776543316fd3fbbe013dc291ce1d5
Pavel Ruban <pasharuban211425@mail.ru>
2021-12-23 20:56:07 +0300

    added select-rows  pagination

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,28 @@
+"use strict";
+
 window.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabs_item");
   const pageContentContainers = document.querySelectorAll("main .container");
 
   const likeButtons = document.querySelectorAll(".like");
+
+  const table = document.querySelector(".table");
+  const select = document.querySelector("select");
+  const minRows = 3;
+  const amountOfColumns = 3;
+
+  const tableRows = [];
+  const cells = [];
+  let selectedRows = select.value;
+
+  const tableHeader = document.createElement("div");
+  tableHeader.className = "table-header table-row";
+
+  tableHeader.innerHTML = `
+    <span>Position</span>
+    <span>Location</span>
+    <span>Price</span>
+    `;
 
   const removePrevActiveClass = () => {
     tabs.forEach((tab) => {
@@ -45,6 +65,41 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   };
 
+  const clearTableContent = () => {
+    while (table.hasChildNodes()) {
+      table.removeChild(table.lastChild);
+    }
+  };
+
+  const createTableHeader = () => {
+    table.appendChild(tableHeader);
+  };
+
+  const createTable = () => {
+    clearTableContent();
+    createTableHeader();
+
+    for (let rows = 0; rows < selectedRows; rows++) {
+      let row = document.createElement("div");
+      row.className = "table-row row-elements";
+
+      for (let columns = 0; columns < amountOfColumns; columns++) {
+        let cellElement = document.createElement("span");
+        cellElement.textContent = `item${rows * minRows + columns}`;
+        row.appendChild(cellElement);
+      }
+      table.appendChild(row);
+    }
+  };
+
+  const selectRows = () => {
+    select.addEventListener("change", (event) => {
+      selectedRows = event.target.value;
+      createTable();
+    });
+  };
+
   selectTab();
   addLike();
+  selectRows();
 });
